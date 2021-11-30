da683a99d324b8b6b463337b863b64203ec596ac
Naseem Shah <naseemshahvarikodan@gmail.com>
2021-11-30 08:44:35 +0530

    create sign-up page.

---
+++ script.js
@@ -0,0 +1,18 @@
+var instance = new vidbg(
+    ".body",
+    {
+        mp4: "website-bg.mp4", // URL or relative path to MP4 video
+        webm: "website-bg.webm", // URL or relative path to webm video
+        poster: "vid_poster", // URL or relative path to fallback image
+        overlay: false, // Boolean to display the overlay or not
+        overlayColor: "#000", // The overlay color as a HEX
+        overlayAlpha: 0.3, // The overlay alpha. Think of this as the last integer in RGBA()
+    },
+    {
+        autoplay: true,
+        controls: false,
+        loop: true,
+        muted: true,
+        playsInline: true,
+    }
+);
